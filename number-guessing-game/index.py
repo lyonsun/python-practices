ba97e6b3253f4d36472c41ca9d264ee65f12dc59
Liang Sun <sunly917@gmail.com>
2024-05-06 22:56:54 +0300

    feat: turn number guessing game into a module

--- number-guessing-game/index.py
+++ number-guessing-game/index.py
@@ -2,52 +2,48 @@ import math
 import random
 
 
-while True:
-    try:
-        lower = int(input("Enter lower bound: "))
-        if lower <= 0:
-            print("Please enter a positive number!")
-        else:
-            break
-    except ValueError:
-        print("Please enter a number!")
-
-while True:
-    try:
-        upper = int(input("Enter upper bound: "))
-        if upper <= lower:
-            print(f"Please enter a number bigger than the lower bound {lower}!")
-        else:
-            break
-    except ValueError:
-        print("Please enter a number!")
-
-answer = random.randint(lower, upper)
-max_count = round(math.log(upper - lower + 1, 2))
-count = 0
-
-print(f"\n\tYou have only {max_count} chances to win the game.\n")
-
-for _ in range(0, max_count):
-    count += 1
-
+def getNumberInput(prompt, min=0):
     while True:
         try:
-            guess = int(input("What is your guess: "))
-            break
+            user_input = int(input(prompt))
+            if user_input <= min:
+                print(f"Please enter a number greater than {min}!")
+            else:
+                break
         except ValueError:
             print("Please enter a number!")
-
-    if guess < answer:
-        print("Your guess is too small, try again.")
-    elif guess > answer:
-        print("Your guess is too large, try again.")
-    else:
-        print(f"Congratulations you made it in {count} try!")
-        break
+    return user_input
 
 
-if count >= max_count:
-    print(
-        f"\n\tYou are running out of {max_count} avaiable chances. The answer is {answer}, better luck next time!\n"
-    )
+def guess():
+    print("\n\tWelcome to the maddy number guess game!\n")
+
+    lower = getNumberInput("Enter lower bound: ")
+    upper = getNumberInput("Enter upper bound: ", lower)
+    answer = random.randint(lower, upper)
+    max_count = round(math.log(upper - lower + 1, 2))
+    count = 0
+
+    print(f"\n\tYou have only {max_count} chances to win the game.\n")
+
+    for _ in range(0, max_count):
+        guess = getNumberInput("What is your guess: ")
+
+        if guess < answer:
+            print("Your guess is too small, try again.")
+        elif guess > answer:
+            print("Your guess is too large, try again.")
+        else:
+            print(f"Congratulations! You made it in {count+1} try!")
+            break
+
+        count += 1
+
+    if count >= max_count:
+        print(
+            f"\n\tYou are running out of {max_count} avaiable chances. The answer is {answer}, better luck next time!\n"
+        )
+
+
+if __name__ == "__main__":
+    guess()
